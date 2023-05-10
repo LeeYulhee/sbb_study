ee4828af1298e2d8b4903f484691e8f9ca0f8e8c
LeeYulhee <reasonoflife39@gmail.com>
2023-05-10 14:18:13 +0900

    user 전체 완료

--- src/main/java/com/mysite/sbb_practice/user/UserSecurityService.java
+++ src/main/java/com/mysite/sbb_practice/user/UserSecurityService.java
@@ -16,22 +16,38 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class UserSecurityService implements UserDetailsService {
+// Spring Security에 등록하여 사용할 UserSecurityService는 Spring Security가 제공하는 UserDetailsService 인터페이스를 구현해야 함(UserDetailsService는 loadUserByUsername 메서드를 구현하도록 강제하는 인터페이스)
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    /* 해당 메서드는 사용자명으로 비밀번호를 조회하여 return하는 메서드 -> 사용자 이름을 입력 받아 사용자의 정보와 권한을 조회하여 Security에서 사용할 수 있는 형태인 UserDetails로 변환해주는 역할
+       Security는 loadUserByUsername 메서드에 의해 리턴된 User 객체의 비밀번호가 화면으로부터 입력받은 비밀번호와 일치하는지를 검사하는 로직을 내부적으로 갖고 있음
+       UserDetails : ID, PW, 권한 정보를 담은 인터페이스 -> User로 return */
         Optional<SiteUser> _siteUser = this.userRepository.findByusername(username);
+        // SiteUser 클래스를 참조한 _siteUser 변수에 userRepository에서 username으로 찾은 siteUser를 대입하는데, Optional을 통해 해당 siteUser가 없는 경우를 대비
         if(_siteUser.isEmpty()) {
+        // 만약 _siteUser가 비어 있다면
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+            // "사용자를 찾을 수 없습니다." 메세지와 함께 UsernameNotFoundException 오류를 발생시킴
         }
         SiteUser siteUser = _siteUser.get();
+        // SiteUser 클래스를 참조한 siteUser 변수에 _siteUser.get으로 객체 할당
         List<GrantedAuthority> authorities = new ArrayList<>();
+        /* GrantedAuthority 클래스를 참조한 authorities 변수를 List로 생성(권한이 여러 개일 수도 있기 때문)
+           GrantedAuthority : 사용자 권한을 나타내는 클래스 */
         if("admin".equals(username)) {
+        // 만약 username(사용자ID)가 admin과 같다면
             authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+            /* authorities(권한 목록)에 관리자 권한을 추가 -> SimpleGrantedAuthority에 UserRole에서 정의한 ADMIN의 값을 넣어 객체를 생성한 뒤에 authorities에 추가
+               SimpleGrantedAuthority : GrantedAuthority 인터페이스를 구현한 클래스로, 해당하는 권한을 추가하여 객체를 생성 */
         } else {
+        // 그 외에
             authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
+            // authorities(권한 목록)에 UserRole에서 정의한 USER의 값을 넣어 생성한 SimpleGrantedAuthority 객체를 생성해 추가
         }
         return new User(siteUser.getUsername(), siteUser.getPassword(), authorities);
+        // siteUser의 username과 password, authorities(권한 목록)을 넣은 User 객체를 생성해서 return
     }
 }
--- src/main/java/com/mysite/sbb_practice/user/UserService.java
+++ src/main/java/com/mysite/sbb_practice/user/UserService.java
@@ -16,20 +16,36 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
+    // SiteUser의 정보를 새로 저장하는 메서드로 username, email, password를 매개변수로 받음
         SiteUser user = new SiteUser();
+        // StieUser 클래스를 참조한 user 변수에 SiteUser 객체를 생성해서 대입
         user.setUsername(username);
+        // user의 Username을 username으로 저장
         user.setEmail(email);
+        // user의 Email을 email로 저장
         user.setPassword(passwordEncoder.encode(password));
+        /* user의 Password를 매개변수로 전달된 password를 passwordEncoder에 encode한 값을 저장
+           passwordEncoder : 사용자의 비밀번호를 안전하게 저장하기 위해 사용되는 인코더 객체로, 보통 비밀번호는 평문이 아니라 알고리즘을 사용해 암호화된 형태로 저장
+                             Spring Security에서는 PasswordEncoder 인터페이스를 제공하여 비밀번호 인코딩과 관련된 작업을 처리
+                             이 인터페이스를 구현한 여러 클래스들이 있으며 BCryptPasswordEncoder, Argon2PasswordEncoder, Pbkdf2PasswordEncoder, SCryptPasswordEncoder 등이 있음
+                             이 프로젝트에서는 SecurityConfig에 BCryptPasswordEncoder를 이용해 passwordEncoder를 구현하고 @Bean을 통해 Bean으로 등록 */
         this.userRepository.save(user);
+        // 위에 저장한 내용을 userRepository에 save로 저장
         return user;
     }
 
     public SiteUser getUser(String username) {
+    // username으로 검색한 SiteUser의 객체를 return 해주는 메서드
         Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+        // SiteUser 클래스를 참조한 siteUser 변수에 userRepository에서 username으로 찾은 siteUser를 대입하는데, Optional을 통해 해당 siteUser가 없는 경우를 대비
         if(siteUser.isPresent()) {
+        // 만약 siteUser의 값이 있다면(true)
             return siteUser.get();
+            // siteUser 객체를 return(그냥 siteUser는 Optional<SiteUser>라서 해당 방식으로 SiteUser 객체 반환)
         } else {
+        // 그 외에
             throw new DataNotFoundException("siteuser not found");
+            // "siteuser not found" 메세지와 함께 DataNotFoundException 오류를 발생시킴
         }
     }
 }
